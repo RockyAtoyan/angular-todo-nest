237209e202e419707f95a06b8e61b626168fa3ae
Rocky <atoyanrobert21@gmail.com>
2024-06-25 15:40:55 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
 import { Request, Response, NextFunction } from 'express';
 
+export const frontUrl = 'https://angular-todo-2fa64.web.app';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: 'https://angular-todo-2fa64.web.app', // Add your frontend URL here
+    origin: frontUrl, // Add your frontend URL here
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: [
       'Cookie',
@@ -19,6 +21,16 @@ async function bootstrap() {
       'Authorization',
     ],
     credentials: true,
+  });
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.setHeader('Access-Control-Allow-Origin', frontUrl);
+    res.setHeader('Vary', 'Origin');
+    res.setHeader('Access-Control-Allow-Header', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+      return res.status(200).json({ status: 'ok' });
+    }
+    next();
   });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());

0deacfa2113a8f33d6e8f20a8c6bd2b4f6816401
Rocky <atoyanrobert21@gmail.com>
2024-06-25 12:42:30 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
     origin: true,
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Access-Control-Allow-Origin',
-    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-    credentials: true,
   });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());

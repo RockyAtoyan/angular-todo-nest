fe29875aeae233e28ab199adccb47bbf0f7d1740
Rocky <atoyanrobert21@gmail.com>
2024-06-25 13:15:46 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import cookieParser from 'cookie-parser';
+import { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -11,6 +12,12 @@ async function bootstrap() {
       methods: 'GET, POST, PUT, DELETE, OPTIONS',
       allowedHeaders: 'Content-Type, Authorization',
     },
+  });
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
   });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());

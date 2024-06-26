8cf1141a3cd33a64e92d5c4d640b06e6dfebb076
Rocky <atoyanrobert21@gmail.com>
2024-06-26 10:09:20 +0300

    add origin "*"

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
 import { Request, Response, NextFunction } from 'express';
 
-const frontUrl = true;
+const frontUrl = '*';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);

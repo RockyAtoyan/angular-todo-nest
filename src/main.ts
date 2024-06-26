73744e1d435b2493de904e6b66ca26bacc4ef72f
Rocky <atoyanrobert21@gmail.com>
2024-06-26 15:56:04 +0300

    add urls to origins

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
 import { Request, Response, NextFunction } from 'express';
 
-const frontUrl = 'https://angular-todo-2fa64.web.app';
+const frontUrl = ['https://angular-todo-2fa64.web.app', "http://localhost:3000", "http://localhost:4200"];
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);

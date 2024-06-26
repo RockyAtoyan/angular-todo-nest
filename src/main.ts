ab98beb4c6fab01f8626937e0746e29e3a8d0500
Rocky <atoyanrobert21@gmail.com>
2024-06-26 10:22:35 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
 import { Request, Response, NextFunction } from 'express';
 
-const frontUrl = '*';
+const frontUrl = 'https://angular-todo-2fa64.web.app';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -20,7 +20,7 @@ async function bootstrap() {
       'Accept',
       'Authorization',
     ],
-    //credentials: true,
+    credentials: true,
   });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
